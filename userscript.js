7cb705106af7e0273d709d93000efe8e4481eaa9
Damian <webnull.www@gmail.com>
2015-05-08 22:11:36 +0200

    Update userscript.js
    Improvements to timing, fetching additional informations

--- userscript.js
+++ userscript.js
@@ -6,9 +6,11 @@
 // @author       Damian Kęska
 // @match        *.jakdojade.pl/*
 // @grant        none
+// @require      http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
-window.jakdojade_userScripts_lastTimerValue = '';
+window.jakdojade_userScripts_lastTimerData = '';
+window.jakdojade_userScripts_lastTimerValue = 0;
 
 window.jakdojade_userScripts_checkTimer = function ()
 {
@@ -17,21 +19,74 @@ window.jakdojade_userScripts_checkTimer = function ()
     
     currentTitle = document.getElementsByTagName('title')[0].innerHTML;
     
-    if (window.jakdojade_userScripts_lastTimerValue != currentTitle)
+    if (window.jakdojade_userScripts_lastTimerData != currentTitle)
     {
-        window.jakdojade_userScripts_lastTimerValue = currentTitle;
+        window.jakdojade_userScripts_lastTimerData = currentTitle;
         
-        tmp = currentTitle.match(/([0-9]+) minut/g);
-        
-        if (tmp)
+        if (currentTitle.indexOf('do odjazdu') > 0 || currentTitle.indexOf('do wyjścia') > 0)
         {
-        } else {
-            tmp = currentTitle.match(/[0-9]{2}:[0-9]{2}/i);
-        }
-        
-        if (currentTitle.indexOf('do odjazdu') > 0)
-        {
-            sendNotification('jakdojade.pl', currentTitle);
+            re = /([0-9]+) min/g; 
+            tmp = re.exec(currentTitle);
+            time = 0;
+
+            if (tmp)
+            {
+                time = (tmp[1] * 60);
+            } else {
+                re = /([0-9]+) do /g; 
+                tmp = re.exec(currentTitle);
+
+                if (tmp)
+                {
+                    time = (tmp[1] * 60).split(':');
+                    time = ((time[0] * 3600) + (time[1] + 60));
+                }
+            }
+            
+            console.log('[jakdojade-notify] Time to leave: ' + time);
+
+            maxNotificationInterval = 60;
+            diff = (window.jakdojade_userScripts_lastTimerValue - time);
+            
+            // if switching to other connection
+            if (diff < 0)
+            {
+                window.jakdojade_userScripts_lastTimerValue = time;
+                diff = 0;
+            }
+
+            if (time >= 3600) // 1h
+                maxNotificationInterval = 900; // 15 minutes
+            else if (time >= 2400) // 40 min
+                maxNotificationInterval = 600;
+            else if (time >= 600) // 10 min
+                maxNotificationInterval = 300;
+            
+            maxNotificationInterval -= 1;
+            console.log('[jakdojade-notify] Diff: ' + diff);
+            console.log('[jakdojade-notify] maxNotificationInterval: ' + maxNotificationInterval);
+
+            if (diff > maxNotificationInterval || !window.jakdojade_userScripts_lastTimerValue)
+            {
+                window.jakdojade_userScripts_lastTimerValue = time;
+                
+                // notification title: bus/train name + destination bus stop
+                if (jQuery('.tripHeaderSelected .GOQMLGODT').length)
+                {
+                    console.log('[jakdojade-notify] It\'s possible to fetch additional informations');
+                    
+                    notificationTitle = 'Wyjście na linię ' +jQuery('.tripHeaderSelected .GOQMLGODT').html();
+                    
+                    if (jQuery(jQuery('.gwt-SuggestBox')[1]).length)
+                    {
+                        notificationTitle += ', cel końcowy podróży: ' +jQuery(jQuery('.gwt-SuggestBox')[1]).val();
+                    }
+                }
+                
+                console.log('[jakdojade-notify] Sending a notification');
+                
+                sendNotification(notificationTitle, currentTitle);
+            }
         }
     }
 }
@@ -59,4 +114,4 @@ function sendNotification(title, message)
   //}
 }
 
-window.setInterval('window.jakdojade_userScripts_checkTimer();', 15000);
+window.setInterval(window.jakdojade_userScripts_checkTimer, 10000);
